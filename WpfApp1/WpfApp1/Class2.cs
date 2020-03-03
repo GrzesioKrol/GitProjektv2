1f2e53545c332b3ecbf61eed2c5f2f1c56858d1e
Student <Student@DESKTOP-GGOJP1D>
2020-03-03 13:51:36 +0100

    Klasa nowa

---
+++ WpfApp1/WpfApp1/Class2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Class2
+    {
+        public double mnozenie(double x,double y)
+        {
+            return x * y;
+        }
+
+        public double dzielenie(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException();
+            }else
+            return x / y;
+        }
+    }
+}
